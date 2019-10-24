095518c73d852a1974fe0ea55f54adce8f4917ef
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2019-10-24 20:00:45 +0100

    show_fft takes filename, but plots

--- show_fft.py
+++ show_fft.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 from scipy import fftpack
 
 points = []
@@ -7,23 +8,26 @@ with open(sys.argv[1], 'rb') as f:
     for line in f:
         points.append(list(map(int, line.split())))
 
-'''
-The dft bins are centered around n * sampling_frequency / number_of_samples
-Here we have conveniently selected a number of samples equal to the sampling
-frequency.
-'''
+x = np.array(points)[:, 1]
+sampling_rate = 1000
 
-axes = np.array([
-    np.array(points)[:, 0],
-    np.array(points)[:, 1],
-    np.array(points)[:, 2]
-])
+def plot():
+    X = fftpack.fft(x)
+    freqs = fftpack.fftfreq(len(x)) * sampling_rate
 
-def field_strengths_for_one_axis(x):
-    strengths = np.abs(fftpack.fft(x))
-    return [strengths[45], strengths[70]]
+    fig, ax = plt.subplots()
 
-for lo in range(0, len(axes[0]) - 1000, 10):
-    field_strengths = np.sum(np.square(np.array(list(map(field_strengths_for_one_axis, axes[:, lo:lo+1000])))), axis=0)
-    distances = field_strengths**(-1/6)
-    print(*distances)
+    ax.stem(freqs, np.abs(X))
+    plt.show()
+
+def spectrogram():
+    from scipy import signal
+
+    freqs, times, Sx = signal.spectrogram(x, fs=sampling_rate, window='hanning', scaling='spectrum')
+
+    plt.pcolormesh(times, freqs, 10 * np.log10(Sx), cmap='viridis')
+    plt.ylabel('frequency [Hz]')
+    plt.xlabel('time [sec]')
+    plt.show()
+
+plot()
