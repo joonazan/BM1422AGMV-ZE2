8eaa929645a80f1c3446ef097650630e3574d276
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2019-10-25 13:44:46 +0100

    draw n circle live plot

--- live_plot.py
+++ live_plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def field_strengths(axes):
     def field_strengths_for_one_axis(x):
         strengths = np.abs(fftpack.fft(x)) / 1000
-        return [strengths[45], strengths[70]]
+        return [strengths[45], strengths[65], strengths[80]]
 
     field_strengths = np.sum(np.square(np.array(list(map(field_strengths_for_one_axis, axes)))), axis=0)
     return field_strengths
@@ -15,11 +15,14 @@ plt.xlim(-1, 1)
 plt.ylim(-1, 1)
 ax.set_aspect(1)
 
-circle = plt.Circle((0,0), fill=False, radius=0.1)
-circle2 = plt.Circle((0.175,0), fill=False, radius=0.1)
+circles = [
+    plt.Circle((0,0), fill=False, radius=0.1),
+    plt.Circle((0.175,0), fill=False, radius=0.1),
+    plt.Circle((0.085, 0.22), fill=False, radius=0.1)
+]
 
-ax.add_artist(circle)
-ax.add_artist(circle2)
+for circle in circles:
+    ax.add_artist(circle)
 
 while True:
     points = []
@@ -32,9 +35,10 @@ while True:
         np.array(points)[:, 2]
     ])
 
-    K = 1
+    K = 2
     distances = K * field_strengths(axes)**(-1/6)
 
-    circle.set_radius(distances[0])
-    circle2.set_radius(distances[1])
+    for c, d in zip(circles, distances):
+        c.set_radius(d)
+
     plt.pause(0.001)
