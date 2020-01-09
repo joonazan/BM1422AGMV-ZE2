f8820b720ddb7ade8c5f3bdcb341deb446747a4d
Saarhelo Joonatan <joonatan.saarhelo.aalto.fi>
2020-01-09 12:01:32 +0200

    fast 2d position calculation with plotting

--- haarukointi/src/main.rs
+++ haarukointi/src/main.rs
@@ -1,4 +1,5 @@
 use arrayvec::ArrayVec;
+use plotters::prelude::*;
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::{Pow, Zero};
 use rustfft::FFTplanner;
@@ -10,7 +11,7 @@ fn field_strengths_for_one_axis(xs: &mut [Complex<f64>]) -> ArrayVec<[f64; 3]> {
 
     [45, 65, 80]
         .iter()
-        .map(|&i| out[i].norm_sqr() / 1000.0)
+        .map(|&i| (out[i] / 1000.0).norm_sqr())
         .collect::<ArrayVec<[_; 3]>>()
 }
 
@@ -47,7 +48,7 @@ struct Rectangle {
 }
 
 fn field_strength(p: &Complex<f64>) -> f64 {
-    100000000000.0 * p.norm_sqr().sqrt().pow(-6)
+    2_000_000_000.0 * p.norm_sqr().sqrt().pow(-6)
 }
 
 impl Rectangle {
@@ -92,34 +93,108 @@ impl Rectangle {
         let mid_y = (self.start.im + self.end.im) / 2.0;
 
         vec![
-            Rectangle{start: self.start, end: Complex::new(mid_x, mid_y)},
-            Rectangle{start: Complex::new(self.start.re, mid_y), end: Complex::new(mid_x, self.end.im)},
-            Rectangle{start: Complex::new(mid_x, mid_y), end: self.end},
-            Rectangle{start: Complex::new(mid_x, self.start.im), end: Complex::new(self.end.re, mid_y)},
+            Rectangle {
+                start: self.start,
+                end: Complex::new(mid_x, mid_y),
+            },
+            Rectangle {
+                start: Complex::new(self.start.re, mid_y),
+                end: Complex::new(mid_x, self.end.im),
+            },
+            Rectangle {
+                start: Complex::new(mid_x, mid_y),
+                end: self.end,
+            },
+            Rectangle {
+                start: Complex::new(mid_x, self.start.im),
+                end: Complex::new(self.end.re, mid_y),
+            },
         ]
     }
 
     fn offset(&self, v: &Complex<f64>) -> Rectangle {
-        Rectangle{start: self.start + v, end: self.end + v}
+        Rectangle {
+            start: self.start + v,
+            end: self.end + v,
+        }
     }
 }
 
 fn main() {
-    let strengths = get_field_strengths_squared();
-    println!("{:?}", strengths);
-    let offsets: [Complex<f64>; 3] = [Complex::new(0.0, 0.0), Complex::new(-5.0, 0.0), Complex::new(-5.0, -5.0)];
+    let root = BitMapBackend::new("plot.png", (1024, 768)).into_drawing_area();
 
-    let mut rects = vec![Rectangle{start: Complex::new(-50.0, -50.0), end: Complex::new(50.0, 50.0)}];
+    root.fill(&WHITE).unwrap();
 
-    for _ in 0..10 {
-        rects = rects.iter().flat_map(|x| x.subdivide().into_iter()).filter(|rect|{
-            for (o, s) in offsets.iter().zip(&strengths) {
-                if !rect.offset(o).contains_field_strength(*s){
-                    return false;
-                }
+    let root = root
+        .titled("Possible Locations", ("sans-serif", 60))
+        .unwrap()
+        .shrink(((1024 - 700) / 2, 0), (700, 700));
+
+    let draw_rect = |r: &Rectangle, color| {
+        let style = ShapeStyle {
+            color,
+            filled: false,
+            stroke_width: 2,
+        };
+
+        fn convert(x: Complex<f64>) -> (i32, i32) {
+            let x = x * 30.0;
+            (x.re as i32 + 300, x.im as i32 + 300)
+        }
+
+        let r = plotters::prelude::Rectangle::new([convert(r.start), convert(r.end)], style);
+        root.draw(&r).unwrap();
+    };
+
+    let offsets: [Complex<f64>; 3] = [
+        Complex::new(0.0, 0.0),
+        Complex::new(-5.0, 0.0),
+        Complex::new(-5.0, -5.0),
+    ];
+    let search_area = Rectangle {
+        start: Complex::new(-10.0, -10.0),
+        end: Complex::new(10.0, 10.0),
+    };
+
+    draw_rect(&search_area, BLACK.to_rgba());
+    offsets.iter().for_each(|s| {
+        let r = Complex::new(0.2, 0.2);
+        draw_rect(
+            &Rectangle {
+                start: s - r,
+                end: s + r,
+            },
+            BLACK.to_rgba(),
+        );
+    });
+
+    for _ in 0..60 {
+        let strengths = get_field_strengths_squared();
+        let mut rects = vec![search_area.clone()];
+
+        for i in 0..100 {
+            let new: Vec<Rectangle> = rects
+                .iter()
+                .flat_map(|x| x.subdivide().into_iter())
+                .filter(|rect| {
+                    for (o, s) in offsets.iter().zip(&strengths) {
+                        if !rect.offset(o).contains_field_strength(*s) {
+                            return false;
+                        }
+                    }
+                    true
+                })
+                .collect();
+
+            if new.is_empty() {
+                println!("{:?}", i);
+                break;
             }
-            true
-        }).collect();
-        println!("{:?}", rects);
+            rects = new;
+        }
+
+        for r in rects {
+            draw_rect(&r, BLUE.to_rgba());
+        }
     }
 }
