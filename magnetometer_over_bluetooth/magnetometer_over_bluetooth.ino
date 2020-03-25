a39559e53c316130a654dc95ec11e468064ef952
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2020-03-25 19:46:23 +0200

    use asynchronous I2C library

--- magnetometer_over_bluetooth/magnetometer_over_bluetooth.ino
+++ magnetometer_over_bluetooth/magnetometer_over_bluetooth.ino
@@ -1,28 +1,17 @@
 #include <FastBLE.h>
-#include <Wire.h>
-
-void write_register(uint8_t slave_address, uint8_t reg, uint8_t val) {
-  Wire.beginTransmission(slave_address);
-  Wire.write(reg);
-  Wire.write(val);
-  Wire.endTransmission();
-}
+#include <I2C_DMAC.h>
 
 void initialize_magnetometer(uint8_t slave_address) {
-  Wire.begin();
-  Wire.setClock(400000);
+  I2C.begin(400000, REG_ADDR_8BIT, PIO_SERCOM_ALT);
 
   // continous 14-bit measurement at 1kHz
-  write_register(slave_address, 0x1b, 0b11011000);
+  I2C.writeByte(slave_address, 0x1b, 0b11011000);
 
-  write_register(slave_address, 0x5c, 0x00);
-  write_register(slave_address, 0x5d, 0x00);
-
-  // turn on DRDY pin
-  write_register(slave_address, 0x1c, 0x0c);
+  I2C.writeByte(slave_address, 0x5c, 0x00);
+  I2C.writeByte(slave_address, 0x5d, 0x00);
 
   // start measuring
-  write_register(slave_address, 0x1d, 0x40);
+  I2C.writeByte(slave_address, 0x1d, 0x40);
 }
 
 struct FieldStrength {
@@ -32,15 +21,8 @@ struct FieldStrength {
 };
 
 struct FieldStrength get_reading(uint8_t slave_address) {
-  Wire.beginTransmission(slave_address);
-  Wire.write(byte(0x10));
-  Wire.endTransmission();
-
-  Wire.requestFrom(slave_address, 6);
   FieldStrength fs;
-  for (int i = 0; i < 6; i++) {
-    reinterpret_cast<uint8_t*>(&fs)[i] = Wire.read();
-  }
+  I2C.readBytes(slave_address, 0x10, reinterpret_cast<uint8_t*>(&fs), sizeof(FieldStrength));
 
   return fs;
 }
@@ -66,7 +48,7 @@ void loop() {
       auto t1 = micros();
 
       field_strength_out.write(get_reading(slave_address));
-      delay(10);
+      delayMicroseconds(400);
       BLE.poll();
    
       auto t2 = micros();
