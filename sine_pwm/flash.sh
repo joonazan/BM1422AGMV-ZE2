9d05df40ac95a725454aefd7aafb77ce181f543f
Saarhelo Joonatan <joonatan.saarhelo.aalto.fi>
2020-07-09 14:35:44 +0300

    sine_pwm: add C++14 compilation flag
    Makes it compile on older compilers

--- sine_pwm/flash.sh
+++ sine_pwm/flash.sh
@@ -1,3 +1,3 @@
-avr-gcc -Wall -Wextra -Werror -O2 -DF_CPU=16000000UL -mmcu=atmega328p sine_pwm.cpp && \
+avr-gcc -Wall -Wextra -Werror -O2 -DF_CPU=16000000UL -mmcu=atmega328p sine_pwm.cpp -std=c++14 && \
 avr-objcopy -O ihex -R .eeprom a.out sine_pwm.hex && \
 avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:sine_pwm.hex
